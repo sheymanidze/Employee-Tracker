d2c874a76565cac16230b3d107d4a041d63e36d6
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-05-12 15:36:00 -0400

    schema added

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS emp_trackerDB;
+CREATE database emp_trackerDB;
+USE emp_trackerDB;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary INTEGER NULL,
+  department_id INTEGER NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NULL,
+  manager_id INTEGER,
+  PRIMARY KEY (id)
+);
