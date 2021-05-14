45cdfd2aa59c741a1940e73e2d1e3de0aab515c8
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-05-14 17:24:20 -0400

    add delete Roles, and Emp

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ const addEmp = () => {
             //     console.table(res)
             //     allEmployees();
             //   })
-            connection.query(`INSERT INTO employee (first_name, role_id, manager_id) VALUES`, [response.first_name, response.last_name, response.role, response.manager], newEmp, (err, res) => {
+            connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES`, [response.first_name, response.last_name, response.role, response.manager], newEmp, (err, res) => {
 
               if (err) throw err;
 
@@ -252,15 +252,19 @@ const addDepartment = () => {
     name: 'department_name',
     message: 'Please add new Department Name'
   }]).then((answer) => {
-    connection.query(`INSERT INTO department VALUES (?)`, (err, res) => {
-      if (err) throw err;
+    connection.query(`INSERT INTO department SET ?)`,
+      {
+        department_name: answer.department_name
 
-      console.table(res);
+      }, (err, res) => {
+        if (err) throw err;
 
-      allDepartments();
-      optionsStart();
+        console.table(res);
 
-    })
+        allDepartments();
+        optionsStart();
+
+      })
   })
 }
 
@@ -299,90 +303,112 @@ const addRole = () => {
 
 //Update Employee Manager (updateEmpManager)
 
+
+
 //View Employee by Manager (empByManager)
-const empByManager = () => {
-  connection.query(`SELECT CONCAT(employee.first_name, ' ' , employee.last_name) AS full_name FROM employee INNER JOIN role ON employee.role_id=role.id
-  INNER JOIN department ON department.id=role.department_id
-  WHERE role.title  IN ('Marketing Manager', 'Sales Manager', 'Account Manager', 'IT Manager', 'Human Resources');`,
-    function (err, res) {
-      let managerList = [];
-      let managerID = {};
+// const empByManager = () => {
+//   connection.query(`SELECT CONCAT(employee.first_name, ' ' , employee.last_name) AS full_name FROM employee INNER JOIN role ON employee.role_id=role.id
+//   INNER JOIN department ON department.id=role.department_id
+//   WHERE role.title  IN ('Marketing Manager', 'Sales Manager', 'Account Manager', 'IT Manager', 'Human Resources');`,
+//     function (err, res) {
+//       let managerList = [];
+//       let managerID = {};
 
-      res.forEach(({ full_name, id }) => {
-        managerList.push(full_name);
-        managerID[full_name] = id;
-      });
+//       res.forEach(({ full_name, id }) => {
+//         managerList.push(full_name);
+//         managerID[full_name] = id;
+//       });
 
-      console.table(res);
+//       console.table(res);
 
 
-      inquirer.prompt([{
-        type: 'list',
-        name: 'byManager',
-        message: 'Please choose the manager to view employees',
-        choices: managerList
-      }]).then((answer) => {
-        let selectedManager = managerID[answer.managerList];
-        connection.query(`SELECT CONCAT(employee.first_name, ' ' , employee.last_name) AS full_name, department_name, role.title FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id=role.department_id WHERE manager_id=?;`,
-          {
-            manager_id: selectedManager
-          },
-          function (err, manager) {
-            console.table(manager)
-          })
-        optionsStart()
+//       inquirer.prompt([{
+//         type: 'list',
+//         name: 'byManager',
+//         message: 'Please choose the manager to view employees',
+//         choices: managerList
+//       }]).then((answer) => {
+//         let selectedManager = managerID[answer.managerList];
+//         connection.query(`SELECT CONCAT(employee.first_name, ' ' , employee.last_name) AS full_name, department_name, role.title FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id=role.department_id WHERE manager_id=?;`,
+//           {
+//             manager_id: selectedManager
+//           },
+//           function (err, manager) {
+//             console.table(manager)
+//           })
+//         optionsStart()
 
-      });
-    })
-}
+//       });
+//     })
+// }
 
 //Delete Employee (removeEmp)
 
-// const removeEmp = () => {
-//   const queryRemEmp = `SELECT * FROM employee`
-//   connection.query(queryRemEmp, (err, res) => {
-//     if (err) throw err;
-//     inquirer.prompt([{
-//       type: 'list',
-//       name: 'employeeID',
-//       message: 'Please select an employee you would like to delete from the list',
-//       choices: res.map(employee = {
-//         return { name: `${employee.first_name} ${employee.last_name}`, value: employee.id }
-//       })
-//     }]).then(answer => {
-//       const queryRemEmp1 = `DELETE FROM employee WHERE ?`
-//       connection.query(queryRemEmp1, [{ id: answer.employeeID }], (err) => {
-//         if (err) throw err;
-//         console.table(res);
-//         optionsStart();
-//       })
+const removeEmp = () => {
+  const queryRemEmp = `SELECT * FROM employee`
+  connection.query(queryRemEmp, (err, res) => {
+    if (err) throw err;
+    inquirer.prompt([{
+      type: 'list',
+      name: 'employeeID',
+      message: 'Please select an employee you would like to delete from the list',
+      choices: res.map(employee => {
+        return { name: `${employee.first_name} ${employee.last_name}`, value: employee.id }
+      })
+    }]).then(answer => {
+      const queryRemEmp1 = `DELETE FROM employee WHERE ?`
+      connection.query(queryRemEmp1, [{ id: answer.employeeID }], (err) => {
+        if (err) throw err;
+        console.table(res);
+        optionsStart();
+      })
 
-//     })
-//   })
+    })
+  })
 
-// };
+};
 
 
 
 //Delete Departments (removeDepartment)
 
 //Delete Roles (removeRole)
-
-//View the total utilized budget of a department (totalBudget)
-const totalBudget = () => {
-  inquirer.prompt([{
-    type: 'rawlist',
-    name: 'debBud',
-    message: 'Please choose the department ID to view total utilized budget',
-    choices: ['CEO', 'Marketing', 'Sales', 'Accounting', 'IT', 'HR']
-  }]).then((answer) => {
-    connection.query('SELECT department_id, title SUM (salary) FROM role GROUP by department_id;', {
-      department_id: answer.depBud
-    },
-      (err, res) => {
+const removeRole = () => {
+  const remRole = '`SELECT * FROM role'
+  connection.query(remRole, (err, res) => {
+    if (err) throw err;
+    inquirer.prompt([{
+      type: 'list',
+      name: 'role',
+      message: 'Please select a  role you would like to delete from the list',
+      choices: res.map(role => {
+        return { name: `${role.title}`, value: role.id }
+      })
+    }]).then(answer => {
+      const remRole2 = `DELETE FROM role WHERE ?`
+      connection.query(remRole2, [{ id: answer.role }], (err) => {
         if (err) throw err;
         console.table(res);
         optionsStart();
       })
-  })
-}
+    })
+  }
+
+//View the total utilized budget of a department (totalBudget)
+const totalBudget = () => {
+    inquirer.prompt([{
+      type: 'rawlist',
+      name: 'debBud',
+      message: 'Please choose the department ID to view total utilized budget',
+      choices: ['CEO', 'Marketing', 'Sales', 'Accounting', 'IT', 'HR']
+    }]).then((answer) => {
+      connection.query('SELECT department_id, title SUM (salary) FROM role GROUP by department_id;', {
+        department_id: answer.depBud
+      },
+        (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          optionsStart();
+        })
+    })
+  }
