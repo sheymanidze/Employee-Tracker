b7eedc370b3bb7c70d30a7aa7664dfd2161dab5d
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-05-13 20:29:38 -0400

    seed updated

--- db/seed.sql
+++ db/seed.sql
@@ -2,12 +2,36 @@ USE emp_trackerDB;
 
 INSERT INTO department (name)
 VALUES
-()
+("Marketing"),
+("Sales"),
+("Accounting"),
+("IT"),
+("HR")
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-()
+("CEO", 350000, Null),
+("Marketing Manager", 100000, 1),
+("Marketing Assistant", 50000, 1),
+("Sales Manager", 150000, 2),
+("Sales Representative", 80000, 2),
+("Account Manager", 85000, 3),
+("Account Assistant", 45000, 3),
+("IT Manager", 90000, 4),
+("Tech Support", 55000, 4),
+("Developer", 110000, 4),
+("Human Resources", 96000, 5)
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
-()
+("Anastasia", "Sorkin", 1, Null),
+("Sky", "Tyler", 1, 2),
+("Sam", "Edwards", 2, 4),
+("Olivia", "Moore", 1, 2),
+("Adrian", "Marshal", 2, 4),
+("Maria", "Stark", 3, 5),
+("Alexander", "Smith", 4, 8),
+("Emmy", "LeBlanc", 4, 8),
+("Oliver", "Johnson", 4, 9),
+("Amanda", "Mirucci", 5, 10),
+("Mathew", "Lewinski", 3, 5)
--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const inquirer = require('inquirer');
-const express = require('express');
-const connect = require('./config/connection.js');
+const connection = require('./config/connection.js');
 const mysql = require('mysql');
-const cTable = require('console.table')
-
-const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const cTable = require('console.table');
 
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
-
+connection.connect(function (err) {
+  if (err) throw err;
+  optionsStart();
+})
 
 function optionsStart() {
   inquirer.prompt({
     type: 'list',
     name: 'start',
     message: 'What would you like to do?',
-    choices: ['View All Employees', 'View All Employess By Depatment', 'View All Emplyees by Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'View All Departments', 'Add New Department', 'Remove Department', 'View All Roles', 'Add New Role', 'Remove Role']
+    choices: ['View All Employees', 'View All Employess By Depatment', 'View All Emplyees by Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'View All Departments', 'Add New Department', 'Remove Department', 'View All Roles', 'Add New Role', 'Remove Role', 'View total utilized budget']
   })
     .then((answer) => {
       switch (answer.start) {
@@ -80,21 +74,73 @@ function optionsStart() {
 };
 
 //View All Employees
-function allEmployees() {
-  const queryEmp = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, role.salary AS salary, CONCAT(manager.first_name, ' ',manager.last_name) AS manager, department.name AS department
-  FROM employee
-  LEFT JOIN role ON employee.role_id=department.id
-  LEFT JOIN employee manager ON employee.manager_id=manager.id`
-
-  connection.query(queryEmp, (err, data) => {
+const allEmployees = () => {
+  connection.query('SELECT * FROM employee', (err, res) => {
     if (err) throw err;
-    console.table(data);
-    optionsStart();
-  })
+    res.forEach(({
+      id,
+      first_name,
+      last_name,
+      role_id,
+      manager_id
+    }) => {
+      console.table(id, first_name, last_name, role_id, manager_id)
+      //console.log(`| ${id} | ${first_name} | ${last_name} | ${role_id} | ${manager_id} |`);
+    });
+    //console.log('----------------------------');
+    optionsStart()
+
+  });
 }
 
+
+
+// function allEmployees() {
+//   const queryEmp = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, role.salary AS salary, CONCAT(manager.first_name, ' ',manager.last_name) AS manager, department.name AS department
+//   FROM employee
+//   LEFT JOIN role ON employee.role_id=department.id
+//   LEFT JOIN employee manager ON employee.manager_id=manager.id`
+
+//   connection.query(queryEmp, (err, data) => {
+//     if (err) throw err;
+//     console.table(data);
+//     optionsStart();
+//   })
+// }
+
 //View All Employess By Depatment
-//function empByDepartment()
+// function empByDepartment() {
+//   const queryByDep = ('SELECT * FROM department');
+
+//   connection.query(queryByDep, (err, answer) => {
+//     if (err) throw err;
+//     const department = response.map(element => {
+//       return { name: `${element.name}` }
+//     });
+//     inquirer.prompt([{
+//       type: 'list',
+//       name: 'depatments',
+//       message: 'Please chose a department',
+//       choices: department
+//     }]).then(answer => {
+//       const queryEmpByDep = `SELECT employee.first_name, employee.last_name, employee.role_id AS role,  CONCAT(manager.first_name, ' ',manager.last_name) AS manager, department.name AS department
+//       FROM employee
+//       LEFT JOIN employee ON employee.role_id=role.id
+//       LEFT JOIN employee manager ON employee.manager_id=manager.id
+//       LEFT JOIN department ON role.department_id=department.id
+//       WHERE ?`
+
+//       connection.query(queryEmpByDep, [{ name: answer.departments }], function (err, res) {
+//         if (err) throw err;
+//         console.table(res);
+//         optionsStart();
+//       })
+//     })
+
+//   })
+// };
+
+//View Employee by Manager
 
 //class mainData {
  // constructor(connection) {
@@ -106,7 +152,7 @@ function allEmployees() {
 //.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, role.salary AS salary, CONCAT(manager.first_name, ' ',manager.last_name) AS manager, department.name AS department
  // FROM employee
  // LEFT JOIN role ON employee.role_id=role.id
-//LEFT JOIN department role.department_id=department.id`)
+//LEFT JOIN department ON role.department_id=department.id`)
 //.catch(err=>err)
 //}
 //}
